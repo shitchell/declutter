d741c73cd8359a11149c27d29d6eedff6e41e67e
Shaun <shauni3dud3@gmail.com>
2020-08-05 19:50:49 -0400

    initial restructuring

--- organizer-2.py
+++ organizer-2.py
@@ -42,12 +42,15 @@ import shutil
 import readline
 from glob import glob
 from pathlib import Path
-from typing import Union, Tuple, Dict, Optional, List
+from typing import Union, Tuple, Dict, Optional, List, Iterable, Callable
 
+class ReservedShortcutException(Exception): pass
+class InputFormatException(Exception): pass
+class EmptyInputException(Exception): pass
 class InvalidKeyException(Exception): pass
 class InvalidShortcutException(Exception): pass
 
-class OrganizedFile(type(Path())):
+class OrganizedFile(type(Path())): # type: ignore
     """
     Subclass of pathlib.Path that tracks whether or not a file has been
     organized and is at its desired location.
@@ -65,7 +68,7 @@ class OrganizedFile(type(Path())):
     def organized(self, is_organized: bool):
         self._is_organized = is_organized
 
-    def iterdir(self, depth: int = 0, recursive: bool = False) -> List[OrganizedFile]:
+    def iterdir(self, depth: int = 0, recursive: bool = False) -> Iterable[OrganizedFile]:
         """
         Iterate over the files in this directory. Does not yield any
         result for the special paths '.' and '..'. Allows for recursively
@@ -107,7 +110,7 @@ class OrganizedFile(type(Path())):
         directory: Path
         if _destination.is_file():
             directory = _destination.parent
-        elif _destination.is_directory():
+        elif _destination.is_dir():
             directory = _destination
 
         # Get the destination filename
@@ -121,24 +124,41 @@ class OrganizedFile(type(Path())):
 
         shutil.move(self.path, destination)
 
+    def delete(self):
+        """
+        Delete the file
+        """
+        remove(self)
+
 class Organizer:
     files: List[OrganizedFile]
     shortcuts: Dict[str, Path]
     _history: History
 
-    def __init__(self, history: Union[str, Path] = ""):
+    def __init__(self, history: Union[str, Path] = None):
         pass
 
-    def move(self, source: Union[str, Path, OrganizedFile], destination: [str, Path], overwrite: bool = False) -> None:
+    def move(self, source: Union[str, Path, OrganizedFile], destination: Union[str, Path], overwrite: bool = False) -> None:
         pass
+
+    def get_shortcut(self, key) -> Optional[Shortcut]:
+        path: Optional[Path] = self.shortcuts.get(key)
+        if path:
+            return Shortcut(key, path)
+        return None
 
 class History:
     _path: str
-    file: List[OrganizedFile]
+    files: List[OrganizedFile]
     shortcuts: Dict[str, Path]
 
-    def __init__(self, path: Union[str, Path]):
-        self.path = path
+    def __init__(self, path: Union[str, Path] = None):
+        self._path = str(path)
+        if not path:
+            self.files = []
+            self.shortcuts = {}
+        else:
+            self.load(path)
 
     @property
     def path(self) -> Path:
@@ -170,6 +190,9 @@ class Shortcut:
         self.key = key
         self.path = Path(path)
 
+    def __str__(self):
+        return f"{self._key}: {_self.path}"
+
     @property
     def key(self) -> str:
         return self._key
@@ -199,13 +222,21 @@ class Shortcut:
         self._path = _path
 
     def as_dict(self) -> Dict[str, Path]:
-        retun {self._key: self._path}
+        return {self._key: self._path}
 
 class TUI:
+    import argparse
+
     options: argparse.Namespace
     _parser: argparse.ArgumentParser
     _history: History
     _organizer: Organizer
+    _key_actions: Dict[str, str] = {
+        "?": "help",
+        "-": "delete",
+        "\r": "preview",
+        "\n": "preview"
+    }
     V_MAX = 6 # Only print if verbose
     V_DEF = 3 # Default verbosity
     V_MIN = 0 # Minimum required for functionality
@@ -219,7 +250,7 @@ class TUI:
         import argparse
 
         # Setup settings
-        self._parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
         #parser.add_argument("-r", "--recursive", help="TODO", action="store_true")
         #parser.add_argument("-d", "--depth", help="TODO", type=int, default=0)
         parser.add_argument("--history", help="TODO", default=Path.home().joinpath(".declutterpy.json"))
@@ -239,7 +270,84 @@ class TUI:
         else:
             self.options = parser.parse_args()
 
-    def printv(*args, level: int = 0, **kwargs):
+    @staticmethod
+    def _shortcut_completer(text: str, state: int) -> Optional[str]:
+        buffer = readline.get_line_buffer()
+        line = readline.get_line_buffer().split()
+
+        # Only autocomplete the second item
+        if len(line) != 2 and not buffer.endswith(" "):
+            return None
+
+        # Get the base directory typed
+        list_dir: str
+        if not line or buffer.endswith(" "):
+            list_dir = "."
+        else:
+            # Current entire filepath
+            cur_path: str = os.path.expanduser(line[-1])
+            list_dir = os.path.dirname(cur_path)
+
+        # Put together the base directory, filename, and an asterisk
+        term = text + "*"
+        query = str(os.path.join(list_dir, term))
+        files = glob(os.path.expanduser(query))
+        matches = [Path(x).stem + os.path.sep for x in files if Path(x).is_dir()]
+        if state < len(matches):
+            return matches[state]
+        else:
+            return None
+
+    def input_shortcut(self, prompt: str = "") -> Dict[str, Path]:
+        """
+        Input that accepts a single character followed by a filepath, eg:
+
+        $ a /path/to/file
+        
+        Provides filepath completion, eg:
+
+        $ p ~/Down<tab>
+        becomes
+        $ p ~/Downloads
+        """
+        readline.set_completer(TUI._shortcut_completer)
+        readline.parse_and_bind("tab: complete")
+
+        line: str = input(prompt)
+        # Empty line given
+        if not line.strip():
+            raise EmptyInputException()
+
+        parts: list = line.split(maxsplit=1)
+
+        # Must be 2 words/parts
+        if len(parts) != 2:
+            raise InputFormatException()
+
+        # Collect the parts
+        char: str = parts[0]
+        filepath: str = parts[1]
+        path: Path = Path(filepath).expanduser()
+
+        # Cannot use reserved keys
+        if char in self._key_actions:
+            raise ReservedShortcutException(char)
+
+        # Shortcut can only be one character
+        if len(char) > 1:
+            raise InputFormatException()
+
+        # Path must exist and be a directory
+        if not path.is_dir():
+            raise NotADirectoryError(path)
+
+        # Directory must be writable
+        if not os.access(path, os.W_OK):
+            raise PermissionError(path)
+
+        return {char: path}
+
+    def printv(self, *args, level: int = 0, **kwargs):
         """
         Prints information based on an integer verbosity level. 3 variables are
         defined to distinguish the main types of output:
@@ -248,9 +356,10 @@ class TUI:
         TUI.V_DEF -> Default setting
         TUI.V_MIN -> Only the minimum required to function
         """
-        if level <= options.level:
+        if level <= self.options.level:
             print(*args, **kwargs)
 
+    @staticmethod
     def getch() -> str:
         """
         Returns a single byte from stdin (not necessarily the full keycode for
@@ -275,6 +384,7 @@ class TUI:
             return "" # handle ctrl+C
         return ch
 
+    @staticmethod
     def getkey() -> str:
         """
         Returns the (full) keycode for a single keypress from standard input
@@ -292,20 +402,104 @@ class TUI:
         c4 = TUI.getch()
         return c1 + c2 + c3 + c4
 
-    def _print_startup_information():
+    def _print_startup_information(self):
         printv()
+ 
+   # Interactive file organization actions
+    def _get_action(self, action):
+        if hasattr(self, "do_" + action):
+            return getattr(self, "do_" + action)
+
+    def do_delete(self, ctx):
+        ctx.get('file').delete()
+
+    def organize_files(self):
+        """
+        Loop over the organizer's list of files, using keyboard commands to
+        manipulate file locations
+        """
+        i: int = 0
+        new_files: bool = False
+        organized_count = 0
+        printv("Type a shortcut key or ?:", level=TUI.V_DEF)
+        for i in range(len(self._organizer.files)):
+            # Never let the file index drop below 0
+            if i < 0:
+                i = 0
+
+            i = True
+
+            # Get the next file
+            file: OrganizedFile = self._organizer.files[i]
+            printv(f"{file} -> ", level=TUI.V_MIN)
+
+            # Get a single keypress from the user
+            valid_key: bool = False
+            while not valid_key:
+                key: str = TUI.getkey()
+
+                # See if there is a shortcut associated with the key
+                action: str
+                shortcut: Shortcut = self._organizer.get_shortcut(key)
+                if shortcut:
+                    action = "move"
+                else:
+                    action = TUI._key_actions.get(key)
+                action_func: Callable = self._get_action(action)
+
+                if func:
+                    try:
+                        action_func(locals())
+                    except Exception as e:
+                        printv(f"[error] {e}", level=TUI.V_MIN)
+                    else:
+                        valid_key = True
+                else:
+                    # key pressed is not associated with an action or shortcut
+                    pass
 
     def run(self):
         """
         Launch a text based interface to organize files
         """
-        # Display loaded information (number of files and shortcuts)
+        # Startup info -> Number of files passed in and saved shortcuts
         if not self.options.skip_setup:
             printv("Processing {} files".format(len(self._organizer.files)), level=V_DEF)
             printv("", level=V_DEF)
             printv("Loaded shortcuts:", level=V_DEF)
-            for shortcut in 
+            for shortcut in self._history.shortcuts:
+                print(f"- {shortcut}")
+
         # Setup shortcuts
+        if not self.options.skip_setup:
+            empty_or_EOF = False
+            while not empty_or_EOF:
+                try:
+                    shortcut = self.input_shortcut("Enter a shortcut and path (empty line when done):")
+                except (EOFError, EmptyInputException):
+                    empty_or_EOF = True
+                except InputFormatException:
+                    printv("Shortcut must be a single character followed by a file path, eg: d ~/Downloads", level=TUI.V_DEF)
+                except InvalidPathException as e:
+                    printv(f"{e} is not a directory", level=TUI.V_DEF)
+                except PermissionError as e:
+                    printv(f"You do not have permission to access {e}", level=TUI.V_DEF)
+                else:
+                    self._organizer.shortcuts.update(shortcut)
+
+        # Start organizing files
+        i: int = 0
+        new_files: bool = False
+        printv("Type a shortcut key or ?:", level=TUI.V_DEF)
+        for i in range(len(self._organizer.files)):
+            # Never let the file index drop below 0
+            if i < 0:
+                i = 0
+
+            i = True
+
+            # Get the next file
+            file: OrganizedFile = self._organizer.files[i]
 
     # Organize files
 if __name__ == '__main__':
